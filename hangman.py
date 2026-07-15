a7c2892216749a13ebef2b1a44b2bd09edd9a732
Jenzy-r2x <jenzymendez@gmail.com>
2026-07-15 16:27:19 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+import random
+
+words = [hello,arizona,mango,apple pie]
+random_word = random.choice(words)
+letters_guessed = []
+lives = 6
+def display_word():
+    return
+
+
+def guessed_word():
+    return
+
+
+
+
+
+
+
+
